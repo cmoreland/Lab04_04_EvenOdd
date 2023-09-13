58448ff3a7183bc4d6cdf9078800b6b4de229460
Cameron <morelacn@mail.uc.edu>
2023-09-13 18:55:54 -0400

    Completed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int oddEven = 6;
+
+        if (oddEven % 2 == 1){
+            System.out.println(oddEven + " is odd.");
+        } else {
+            System.out.println(oddEven + " is even.");
+        }
+    }
+}
